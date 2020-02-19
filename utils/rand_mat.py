96aefef9f8340d9ea8254a84fa29f29ae502f1c7
Janosh Riebesell <janosh.riebesell@gmail.com>
2020-02-19 14:36:12 +0000

    refactor: rand_mat remove redundant std_init options

--- utils/rand_mat.py
+++ utils/rand_mat.py
@@ -2,29 +2,25 @@ import numpy as np
 import tensorflow as tf
 
 
-def rand_mat(shape, mu=0, var_init="he2", var_scale=1, **kwargs):
+def rand_mat(shape, mean=0, stddev=1, std_init="glorot", **kwargs):
     if len(shape) == 1:
-        dim_in, dim_out = shape[0], 0
+        n_in, n_out = shape[0], 0
     elif len(shape) == 2:
-        dim_in, dim_out = shape
+        n_in, n_out = shape
     else:
-        dim_in, dim_out = np.prod(shape[1:]), shape[0]
+        n_in, n_out = np.prod(shape[1:]), shape[0]
 
     # Xavier Glorot initialization (http://proceedings.mlr.press/v9/glorot10a)
-    if var_init == "glorot":
-        bound = np.sqrt(1 / dim_in)
-    elif var_init == "glorot2":
-        bound = np.sqrt(2 / (dim_in + dim_out))
+    if std_init == "glorot":
+        scale = np.sqrt(2 / (n_in + n_out))
     # Kaiming He initialization (https://arxiv.org/abs/1502.01852)
-    elif var_init == "he":
-        bound = np.sqrt(2 / dim_in)
-    elif var_init == "he2":
-        bound = np.sqrt(4 / (dim_in + dim_out))
-    elif var_init == "regular":
-        bound = 0.01  # sigma_init
+    elif std_init == "he":
+        scale = np.sqrt(2 / n_in)
+    elif std_init == "const":
+        scale = 0.01
     else:
-        raise ValueError("Unknown variance initialization for rand_mat")
+        raise ValueError(f"Unknown value {std_init} for std_init")
 
     # actual weight initialization
-    init_val = tf.random.normal(shape, mean=mu, stddev=var_scale * bound)
+    init_val = tf.random.normal(shape, mean=mean, stddev=stddev * scale)
     return tf.Variable(init_val, **kwargs)
