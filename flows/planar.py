295f8b6bf751f550912ebab407e7a4e2291c7256
Janosh Riebesell <janosh.riebesell@gmail.com>
2020-02-20 13:59:32 +0000

    refactor: simplify RNVP, PlanarFlow further using Keras

--- flows/planar.py
+++ flows/planar.py
@@ -12,28 +12,21 @@ class PlanarFlow(tf.Module):
     def __init__(self, dim, **kwargs):
         super().__init__(**kwargs)
         glorot = tf.keras.initializers.GlorotNormal()
-        w = tf.Variable(glorot([dim, 1]))
-        u = tf.Variable(glorot([dim, 1]))
-        b = tf.Variable(tf.zeros(1))
-        self.params = [w, u, b]
+        self.u = tf.Variable(glorot([dim, 1]))
+        self.dense = tf.keras.layers.Dense(1)
 
-    def forward(self, z):
-        w, u, b = self.params
+    def forward(self, z):  # z -> x
+        w = self.dense.kernel
+        uw = tf.reduce_sum(self.u * w)
+        suw = -1 + tf.math.softplus(uw)  # = -1 + log(1 + exp(uw))
+        u_hat = self.u + (suw - uw) * w / tf.reduce_sum(w ** 2)
 
-        uw = tf.reduce_sum(u * w)
-        muw = -1 + tf.math.softplus(uw)  # = -1 + log(1 + exp(uw))
-        u_hat = u + (muw - uw) * w / tf.reduce_sum(w ** 2)
-        if len(z.shape) == 1:
-            zwb = z * w + b
-        else:
-            zwb = tf.matmul(z, w) + b
-
-        # We choose tf.tanh as non-linearity.
-        z_shift = tf.matmul(tf.tanh(zwb), tf.transpose(u_hat))
-        z += z_shift
+        zwb = self.dense(z)
+        shift = tf.matmul(tf.tanh(zwb), tf.transpose(u_hat))  # Use tanh non-linearity.
+        x = z + shift
 
         # d tanh(x)/dx = 1 - tf.tanh(x)^2
         psi = tf.matmul(1 - tf.tanh(zwb) ** 2, tf.transpose(w))
         psi_u = tf.matmul(psi, u_hat)
         logdet = tf.squeeze(tf.math.log(tf.abs(1 + psi_u)))
-        return z, logdet
+        return x, logdet
--- flows/rnvp.py
+++ flows/rnvp.py
@@ -2,52 +2,33 @@ import tensorflow as tf
 
 
 class RNVP(tf.Module):
-    """Affine half (aka real non-volume preserving) flow (x = z * exp(s) + t),
-    where a randomly selected half of the dimensions in x are linearly
-    scaled/transfromed as a function of the other half.
+    """Affine half (aka Real Non-Volume Preserving) flow (x = z * exp(s) + t),
+    where a randomly selected half z1 of the dimensions in z are affinely transformed
+    as a function of the other half z2, i.e. scaled by s(z2) and shifted by t(z2).
 
-    From "Density estimation using Real NVP", Dinh et al. (May 2016).
+    From "Density estimation using Real NVP", Dinh et al. (May 2016)
     https://arxiv.org/abs/1605.08803
     """
 
-    def __init__(self, dim, h_layers=0, dim_h=10, nonlin=tf.tanh, **kwargs):
+    def __init__(self, dim, h_sizes=[30], activation="tanh", **kwargs):
         super().__init__(**kwargs)
-        self.params = []
-        # shape of self.params: [W1, b1], [W2, b2], ..., [W_mu, b_mu, W_sigma, b_sigma]
-        # The number of weight-bias pairs [W1, b1], ... is 1 + h_layers.
-        self.nonlin = nonlin
-
-        glorot = tf.keras.initializers.GlorotNormal()
-        w = tf.Variable(glorot([dim, dim_h]))
-        b = tf.Variable(tf.zeros(dim_h))
-        self.params.append([w, b])
-        for l in range(h_layers):
-            wh = tf.Variable(glorot([dim_h, dim_h]))
-            bh = tf.Variable(tf.zeros(dim_h))
-            self.params.append([wh, bh])
-        w_mu = tf.Variable(glorot([dim_h, dim]))
-        b_mu = tf.Variable(tf.zeros(dim))
-        w_sigma = tf.Variable(glorot([dim_h, dim]))
-        b_sigma = tf.Variable(tf.ones(dim))
-        self.params.append([w_mu, b_mu, w_sigma, b_sigma])
-
-    def feed_forward(self, x):
-        for [w, b] in self.params[:-1]:
-            h = tf.matmul(x, w) + b
-            x = self.nonlin(h)
-        w_mu, b_mu, w_sigma, b_sigma = self.params[-1]
-        mu = tf.matmul(x, w_mu) + b_mu
-        sigma = tf.matmul(x, w_sigma) + b_sigma
-        return mu, sigma
+        layers = [tf.keras.layers.Dense(hs, activation) for hs in h_sizes]
+        self.net = tf.keras.Sequential(layers)
+        self.t = tf.keras.layers.Dense(dim)
+        self.s = tf.keras.layers.Dense(dim)
 
     def forward(self, z):  # z -> x
         # Get random Bernoulli mask. This decides which channels will remain
         # unchanged and which will be transformed as functions of the unchanged.
         mask = tf.keras.backend.random_binomial(z.shape, p=0.5)
+        z1, z2 = (1 - mask) * z, mask * z
+        y = self.net(z2)
+        shift = self.t(y)
+        scale = self.s(y)
 
-        mu, sigma = self.feed_forward(mask * z)
-        gate = tf.nn.sigmoid(sigma)
+        # sigmoid(x) = 1 / (1 + exp(-x)). For x in (-inf, inf) => sigmoid(x) in (0, 1).
+        gate = tf.sigmoid(scale)
         log_dets = tf.reduce_sum((1 - mask) * tf.math.log(gate), axis=1)
-        z = (1 - mask) * (z * gate + (1 - gate) * mu) + mask * z
+        x = (z1 * gate + (1 - gate) * shift) + z2
 
-        return z, log_dets
+        return x, log_dets
